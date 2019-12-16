bcec709ff2d797f60ad007008537b9e927e29e7e
Vincent <vincent.stragier97@gmail.com>
2019-12-16 17:37:08 +0100

    Update load_and_process.py

--- load_and_process.py
+++ load_and_process.py
@@ -8,6 +8,8 @@ from sklearn.decomposition import PCA
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import Perceptron
+from sklearn.metrics import confusion_matrix
+from sklearn.neural_network import MLPClassifier
 
 
 # Load the data and convert them to numpy files fdataX.npy and flabels.npy 
@@ -37,12 +39,17 @@ def load_and_process(datafile='./fer2013.csv', images_key='pixels', labels_key='
     # Extract the training, validation and testing set from the data
     train_in  = X[usage == 'Training']
     train_out = y[usage == 'Training']
+    # print(len(train_in), len(train_out))
 
     valid_in  = X[usage == 'PrivateTest']
+    print(valid_in.shape)
     valid_out = y[usage == 'PrivateTest']
+    print(valid_out.shape)
+    # print(len(valid_in), len(valid_out))
 
     test_in   = X[usage == 'PublicTest']
     test_out  = y[usage == 'PublicTest']
+    # print(len(test_in), len(test_out))
 
     print('Shuffle the sets')
     # Shuffling the training database in order to improve the efficiency 
@@ -57,12 +64,14 @@ def load_and_process(datafile='./fer2013.csv', images_key='pixels', labels_key='
     # Normalise the input variables
     scaler_train    = preprocessing.MinMaxScaler(feature_range=(-128,127)).fit(train_in)
     scaled_train_in = scaler_train.transform(train_in)
-
+    
     scaler_valid    = preprocessing.MinMaxScaler(feature_range=(-128,127)).fit(train_in)
-    scaled_valid_in = scaler_valid.transform(train_in)
+    scaled_valid_in = scaler_valid.transform(valid_in)
+    # print(scaled_valid_in.shape)
 
     scaler_test     = preprocessing.MinMaxScaler(feature_range=(-128,127)).fit(train_in)
-    scaled_test_in  = scaler_test.transform(train_in)
+    scaled_test_in  = scaler_test.transform(test_in)
+    # print(len(scaled_train_in), len(scaled_valid_in))
 
     print('PCA analysis')
     # PCA analysis
@@ -71,6 +80,7 @@ def load_and_process(datafile='./fer2013.csv', images_key='pixels', labels_key='
     pca_train = model_pca.transform(scaled_train_in)
 
     pca_valid = model_pca.transform(scaled_valid_in)
+    print('PCA VALID SHAPE' , pca_valid.shape)
 
     pca_test  = model_pca.transform(scaled_test_in)
 
@@ -83,13 +93,60 @@ def load_and_process(datafile='./fer2013.csv', images_key='pixels', labels_key='
     scaled_pca_valid = scaler.transform(pca_valid)  # doctest: +SKIP
     scaled_pca_test = scaler.transform(pca_test)  # doctest: +SKIP
 
+    CLASSES_NAME = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
     # Single Perceptron
+    print('Train perceptron')
     clf = Perceptron(fit_intercept=False,
-                     max_iter=10,
+                     max_iter=1000,
                      tol=1e-3,
                      shuffle=False).fit(scaled_pca_train, train_out)
-    print('Save the results of the PCA analysis')
-    # Save the results
+    print('End training')
+
+    # print(clf.predict(scaled_pca_train))
+    print(clf.score(scaled_pca_train, train_out))
+
+    train_pred        = clf.predict(scaled_pca_train)
+    conf_matrix_train = confusion_matrix(train_out, train_pred)
+    print(conf_matrix_train)
+
+    # print(clf.predict(scaled_pca_valid))
+    # print(scaled_pca_valid.shape, valid_out.shape)
+    print(clf.score(scaled_pca_valid, valid_out))
+    valid_pred        = clf.predict(scaled_pca_valid)
+    conf_matrix_valid = confusion_matrix(valid_out, valid_pred)
+    print(conf_matrix_valid)
+
+    # print(clf.predict(scaled_pca_test))
+    print(clf.score(scaled_pca_test, test_out))
+    test_pred        = clf.predict(scaled_pca_test)
+    conf_matrix_test = confusion_matrix(test_out, test_pred)
+    print(conf_matrix_test) 
+    
+    # MLP
+    print('Train MLP')
+    clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(15,), random_state=1).fit(scaled_pca_train, train_out)
+    print('End training')
+
+    # print(clf.predict(scaled_pca_train))
+    print(clf.score(scaled_pca_train, train_out))
+
+    train_pred        = clf.predict(scaled_pca_train)
+    conf_matrix_train = confusion_matrix(train_out, train_pred)
+    print(conf_matrix_train)
+
+    # print(clf.predict(scaled_pca_valid))
+    # print(scaled_pca_valid.shape, valid_out.shape)
+    print(clf.score(scaled_pca_valid, valid_out))
+    valid_pred        = clf.predict(scaled_pca_valid)
+    conf_matrix_valid = confusion_matrix(valid_out, valid_pred)
+    print(conf_matrix_valid)
+
+    # print(clf.predict(scaled_pca_test))
+    print(clf.score(scaled_pca_test, test_out))
+    test_pred        = clf.predict(scaled_pca_test)
+    conf_matrix_test = confusion_matrix(test_out, test_pred)
+    print(conf_matrix_test)
+
     """
     np.save('modXtrain', pca_train)
     np.save('modytrain', train_out)
